9d0b73ed579dc528c2fe1bc3e05c618cff0c2953
gboned <gboned@cifpfbmoll.eu>
2020-05-12 21:54:44 +0200

    Añadir función con un parámetro para devolver edad

--- js/funciones.js
+++ js/funciones.js
@@ -2,10 +2,15 @@
 //          Creando funciones            //
 /* ------------------------------------- */
 
+// Variables globales, la primera devuelve la fecha, la segunda
+// devuelve el año actual, a partir de la primera.
+var fecha = new Date();
+var añoActual = fecha.getFullYear();
+
 // Función con parámetro para devolver el doble de un número.
-function doble(a) {
+function doble(numero) {
     // Variable local.
-    var resultado = a * 2;
+    var resultado = numero * 2;
     return resultado;
 }
 
@@ -15,4 +20,11 @@ function saludo() {
     // Variable local
     var respuesta = window.prompt("Introduce tu nombre", "Nombre");
     console.log("Hola, " + respuesta + " ¿Qué tal estás?");
+}
+
+// Función con un parámetro, que usa una variable global
+// definida al principio del fichero, solo para mostrar un ejemplo
+// de uso de las funciones globales, aunque en este caso sería mejor que fueran locales.
+function edad(año) {
+    return añoActual - año;
 }
