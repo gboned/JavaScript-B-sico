51cf0d7ebfd46d8fbdc979f08176374d60376fbf
gboned <gboned@cifpfbmoll.eu>
2020-05-12 21:21:16 +0200

    Añadir función con parámetro

---
+++ js/funciones.js
@@ -0,0 +1,10 @@
+/* ------------------------------------- */
+//          Creando funciones            //
+/* ------------------------------------- */
+
+// Función con parámetro para devolver el doble de un número.
+function doble(a) {
+    var resultado = a * 2;
+    return resultado;
+}
+
